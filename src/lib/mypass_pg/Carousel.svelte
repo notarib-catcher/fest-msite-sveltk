d929e336057148f58f4b328bd054fb03233ffb66
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-04-08 23:43:18 +0530

    Added empty array checking, cleaned up code and added a sample ticket to see QR size

--- src/lib/mypass_pg/Carousel.svelte
+++ src/lib/mypass_pg/Carousel.svelte
@@ -2,7 +2,7 @@
 // @ts-nocheck
 
     /**
-     * @type {Array<string>}
+     * @type {Array<string> | null}
      */
     export let codeDataArr;
     import QRCode from "../../lib/common/QRJS.svelte"
@@ -17,19 +17,18 @@
 
     $: currentData = codeDataArr[cursor]
 </script>
+<div class=" flex items-center justify-center h-screen w-screen text-white">
+    {#if !codeDataArr.length || codeDataArr.length == 0}
+        <div>
+            No passes to display
+        </div>
+    {:else}
+        <div class=" mx-2 px-2 absolute text-white">
+            <button on:click={next}>next</button>
+                {#key currentData}
+                    <QRCode codeValue = {currentData} squareSize = {300}/>
+                {/key}
+        </div>
+    {/if}
+</div>
 
-{#if codeDataArr.length == 0}
-<div class=" flex items-center justify-center h-screen w-screen">
-    <div>    No passes to display
-    </div>
-</div>
-{:else}
-<div class=" flex items-center justify-center h-screen w-screen">
-<div class=" mx-2 px-2 absolute text-white">
-    <button on:click={next}>next</button>
-    {#key currentData}
-        <QRCode codeValue = {currentData} squareSize = {300}/>
-    {/key}
-</div>
-</div>
-{/if}
