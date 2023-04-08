21208ad8eee253e3a65d16de25ab6755046d6183
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-04-08 20:26:41 +0530

    Added banner, with fade in and countdown timer

--- src/lib/common/Navbar.svelte
+++ src/lib/common/Navbar.svelte
@@ -2,18 +2,16 @@
     // @ts-ignore
     import NavbarButton from "./NavbarButton.svelte";
     import { page } from '$app/stores';
-    import logo from "../../assets/logo.png"
+
    // @ts-ignore
 </script>
 
 
 <div id="nav-container" class=" w-screen sticky top-0">
     
-    <nav class=" w-full h-full bg-zinc-800  flex flex-row-reverse">
+    <nav class=" w-full h-full bg-zinc-900  flex flex-row-reverse">
         <div class=" h-fit w-full px-4 my-auto order-1 font-extralight text-white align-middle text-xl flex-row flex">
-            <div class="border-r-2 border-x-zinc-600 mr-5 h-full">
-                <img src = {logo} alt = "logo" class=" h-10 mr-6 my-2  ">
-            </div>
+            
             
             <div class=" my-auto">
                 {#if $page.data.session}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
     import "../app.css"
     import Navbar from "../lib/common/Navbar.svelte";
+    import Banner from "../lib/home_pg/Banner.svelte";
     import { page } from '$app/stores';
 </script>
 {#if $page.url.pathname == "/"}
     <div id="banner-container">
-        Big ass pass on home page here
+        <Banner />
     </div>
 {/if}
 <Navbar />
