5c84b20cb4fef680499fdac35f8dac260e4900b7
Chandan <chandancvp08@gmail.com>
2023-04-09 15:10:13 +0530

    got it working on my local

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,1 +1,10 @@
-events page
+
+<script>
+     import { page } from '$app/stores';
+</script>
+
+<div class=" text-white flex flex-col items-center justify-center h-screen">
+    <h1 class="text-6xl font-bold">
+      This is the events page
+    </h1>
+</div>
