4c294e66f1f6d5dba1bed1d6dac013b956bd80e3
Chandan <chandancvp08@gmail.com>
2023-04-11 12:12:06 +0530

    done with events page serverside code

---
+++ src/lib/common/EventTile.svelte
@@ -0,0 +1,15 @@
+<script>
+// @ts-nocheck
+
+	/**
+	 * @type {{ image: any; }}
+	 */
+	 export let data;
+</script>
+
+<div class="p-5 border w-fit">
+    <img src={data.image} alt="event image" width={220} height={220}/>
+	<p>{data.name}</p>
+	<p>{data.pass}</p>
+	<p>{data.slug}</p>
+</div>
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,10 +1,20 @@
 
 <script>
-     import { page } from '$app/stores';
+// @ts-nocheck
+
+	import EventTile from '$lib/common/EventTile.svelte';
+import { each } from 'svelte/internal';
+
+     export let data;
 </script>
 
-<div class=" text-white flex flex-col items-center justify-center h-screen">
-    <h1 class="text-6xl font-bold">
-      This is the events page
+<div>
+    <h1 class="text-6xl font-bold bg-gradient-to-tr  from-[#3BACC1] to-[#2D6DB1] ont-extrabold text-transparent bg-clip-text">
+      Gold Events
     </h1>
+    <p class="text-white">
+    {#each data.events as event}
+      <EventTile data={event}/>
+    {/each}
+    </p>
 </div>
