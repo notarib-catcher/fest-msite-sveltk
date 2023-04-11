c8968904de30f37785dfdfb32a01e4cb8a16314b
Chandan <chandancvp08@gmail.com>
2023-04-11 13:02:05 +0530

    basic event tile styling done... TODO Responsiveness

--- src/lib/common/EventTile.svelte
+++ src/lib/common/EventTile.svelte
@@ -5,16 +5,19 @@
 	 * @type {{ image: any; }}
 	 */
 	 export let data;
+	 import SolsticeFullLogo from '../../../src/assets/SolsticeFullLogo.svg'
 </script>
 
-<div class="p-5 border w-fit rounded-2xl flex justify-center flex-col items-center text-center">
-    <img src={data.image} alt="event image" class="rounded-3xl" width={220} height={220}/>
-	<p>{data.name}</p>
-	<p>{data.pass}</p>
-	<div class="w-[220px]">
+<div class="m-10 p-5 border-2 border-yellow-600  w-fit rounded-2xl flex justify-center flex-col items-center text-center text-white">
+	<div class="flex w-full justify-start mb-2">
+		<img src={SolsticeFullLogo} alt="SolsticeLogoMITBLR" /> 
+	</div>
+    <img src={data.image} alt="eventImage" class="rounded-3xl" width={220} height={220}/>
+	<p class="text-2xl font-bold">{data.name}</p>
+	<div class="w-[220px] m-2 text-xl font-light">
 		<p>{data.slug}</p>
 	</div>
-	<div>
-		<button class="bg-yellow-700" on:click={()=>{}}>Register</button>
+	<div class="w-full flex justify-end mt-2">
+		<button class=" bg-gradient-to-tr from-[#D2691E] to-[#FFD700] hover:bg-blue-700 text-black font-sans py-2 px-4 rounded-xl" on:click={()=>{}}>Register</button>
 	</div>
 </div>
