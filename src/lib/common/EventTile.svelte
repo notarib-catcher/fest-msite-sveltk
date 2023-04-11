8c7f71a981512651ad79c9217d5fcdb3283594d4
Chandan <chandancvp08@gmail.com>
2023-04-11 12:29:19 +0530

    basid styling of events tile

--- src/lib/common/EventTile.svelte
+++ src/lib/common/EventTile.svelte
@@ -7,9 +7,14 @@
 	 export let data;
 </script>
 
-<div class="p-5 border w-fit">
-    <img src={data.image} alt="event image" width={220} height={220}/>
+<div class="p-5 border w-fit rounded-2xl flex justify-center flex-col items-center text-center">
+    <img src={data.image} alt="event image" class="rounded-3xl" width={220} height={220}/>
 	<p>{data.name}</p>
 	<p>{data.pass}</p>
-	<p>{data.slug}</p>
+	<div class="w-[220px]">
+		<p>{data.slug}</p>
+	</div>
+	<div>
+		<button class="bg-yellow-700" on:click={()=>{}}>Register</button>
+	</div>
 </div>
