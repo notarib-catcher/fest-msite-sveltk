375cfd85f60f9db103bdf0669517b1b9121479a6
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-04-12 13:15:22 +0530

    Unified endpoint for expired and cancelled payments

--- src/routes/api/webhook/+server.js
+++ src/routes/api/webhook/+server.js
@@ -1,6 +1,5 @@
 import { error } from '@sveltejs/kit';
 import * as dotenv from 'dotenv' ;
-import  Razorpay  from 'razorpay'
 import { validateWebhookSignature } from 'razorpay/dist/utils/razorpay-utils';
 dotenv.config()
 
@@ -9,18 +8,16 @@ import { MongoClient } from 'mongodb';
 const cstring = process.env.MONGO_URL || ""
 const client = new MongoClient(cstring);
 const database = client.db(process.env.MONGO_DB_NAME);
-const passes = database.collection('passes')
 const payments = database.collection('payments')
 const secret = process.env.RAZORPAY_WEBHOOK_SECRET || ""
 
 // @ts-ignore
-var razorInstance = new Razorpay({ key_id: process.env.RAZORPAY_KEY_ID , key_secret: process.env.RAZORPAY_SECRET })
 
 
 
 /**
  * 
- * @param {import("../../../$types").RequestEvent} request 
+ * @param {import("../../$types").RequestEvent} request 
  */
 export const POST = async ({request}) => {
 
@@ -28,21 +25,25 @@ export const POST = async ({request}) => {
     const reqOb = await request.json()
     console.log("validating...")
     console.log(reqOb)
+
+    let validRequest = false
     try{
-        const validRequest = validateWebhookSignature(JSON.stringify(reqOb), signature, secret)
-        if(!validRequest){
-            throw error(403,"Invalid Signature")
-        }
+        validRequest = validateWebhookSignature(JSON.stringify(reqOb), signature, secret)
     }
-    catch(error){
-        console.error(error)
+    catch(ierror){
+        console.error(ierror)
         throw error(500,"Signature Verifcation Failure")
     }
-    
-    
-    console.log("valid webhook")
-    console.log(reqOb)
 
-    return new Response({})
+    if(!validRequest){
+        console.log("Unable to verify webhook")
+        throw error(403,"Invalid Signature")
+    }
+    
+    
+    await payments.findOneAndUpdate({p_id : { $eq : reqOb.payload.payment_link.entity.id}}, { $set: {status: reqOb.payload.payment_link.entity.status}})
+    
+    
+    return new Response(null)
 
 }
---
+++ src/routes/api/webhook/paid/+server.js
@@ -0,0 +1,49 @@
+import { error } from '@sveltejs/kit';
+import * as dotenv from 'dotenv' ;
+import  Razorpay  from 'razorpay'
+import { validateWebhookSignature } from 'razorpay/dist/utils/razorpay-utils';
+dotenv.config()
+
+import { MongoClient } from 'mongodb';
+
+const cstring = process.env.MONGO_URL || ""
+const client = new MongoClient(cstring);
+const database = client.db(process.env.MONGO_DB_NAME);
+const passes = database.collection('passes')
+const payments = database.collection('payments')
+const secret = process.env.RAZORPAY_WEBHOOK_SECRET || ""
+
+// @ts-ignore
+var razorInstance = new Razorpay({ key_id: process.env.RAZORPAY_KEY_ID , key_secret: process.env.RAZORPAY_SECRET })
+
+
+
+/**
+ * 
+ * @param {import("../../../$types").RequestEvent} request 
+ */
+export const POST = async ({request}) => {
+
+    const signature = request.headers.get('X-Razorpay-Signature') || "nosig"
+    const reqOb = await request.json()
+    console.log("validating...")
+    console.log(reqOb)
+    try{
+        console.log("STRINGIFY: " + JSON.stringify(reqOb))
+        const validRequest = validateWebhookSignature(JSON.stringify(reqOb), signature, secret)
+        if(!validRequest){
+            throw error(403,"Invalid Signature")
+        }
+    }
+    catch(error){
+        console.error(error)
+        throw error(500,"Signature Verifcation Failure")
+    }
+    
+    
+    console.log("valid webhook")
+    
+    
+    return new Response({})
+
+}
