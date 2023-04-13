9db7620ffdaf1881b970030ab3934fc2903d894a
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-04-13 21:12:02 +0530

    Analytics moved to its own component

---
+++ src/lib/common/Analytics.svelte
@@ -0,0 +1,31 @@
+<script>
+// @ts-nocheck
+
+    import { page } from '$app/stores'
+  
+    $: {
+      if (typeof gtag !== 'undefined') {
+        gtag('config', 'G-8KMYR18X52', {
+          page_title: document.title,
+          page_path: $page.url.pathname,
+        })
+      }
+    }
+  </script>
+  
+  <svelte:head>
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=MEASUREMENT_ID">
+    </script>
+    <script>
+      window.dataLayer = window.dataLayer || []
+  
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+  
+      gtag('js', new Date())
+      gtag('config', 'MEASUREMENT_ID')
+    </script>
+  </svelte:head>
