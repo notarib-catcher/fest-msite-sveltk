fecb4a60e75687c26a12a63fdfc2b2c3ff886272
Neat11 <hello.astitva@gmail.com>
2023-04-16 17:58:26 +0530

    responsive updates

--- src/routes/events/[id]/+page.svelte
+++ src/routes/events/[id]/+page.svelte
@@ -2,7 +2,7 @@
 	export let data;
 </script>
 
-<div class="text-white flex h-full items-center flex-col pt-[5vh]">
+<div class="text-white flex h-full items-center  flex-col py-[5vh]">
 	<h3 class="m-[5vh] w-max text-center h-full font-poppins bg-gradient-to-tr font-bold from-[#3BACC1] via-[#2D6DB1] to-[#3BACC1] bg-clip-text text-transparent text-[6vh]">{data.event?.name}</h3>
 	<div class="flex flex-row flex-wrap md:flex-nowrap mt-10 justify-evenly w-[95vw] h-full">
 		<div class="flex flex-col w-[40vw] mx-7 flex-grow-[1] justify-evenly items-end">
@@ -12,11 +12,11 @@
 				>Register</button
 			>
 		</div>
-		<div class="flex flex-col px-6 flex-grow-[1]">
+		<div class="flex flex-col mt-10 sm:mt-0 px-6 flex-grow-[1]">
 			<h1 class="text-[3vh] font-poppins mb-5">About the Event:</h1>
-			<h3 class="ml-8 w-[45vw] mb-8">{data.event?.longDescription}</h3>
+			<h3 class="ml-8 sm:w-[45vw] mb-8">{data.event?.longDescription}</h3>
 			<h1 class="text-[3vh] font-poppins mb-5">Rules:</h1>
-			<h3 class="ml-8 w-[45vw] mb-16">{data.event?.rules}</h3>
+			<h3 class="ml-8 sm:w-[45vw] mb-16">{data.event?.rules}</h3>
 			<h3 class="bg-clip-text text-transparent bg-gradient-to-r from-[#125ddf] to-[#F365E5] w-max font-poppins font-normal text-[3vh]">Date: </h3>
 			<h3 class="bg-clip-text text-transparent bg-gradient-to-r from-[#125ddf] to-[#F365E5] w-max font-poppins font-normal text-[3vh]">Time: </h3>
 			<h3 class="bg-clip-text text-transparent bg-gradient-to-r from-[#125ddf] to-[#F365E5] w-max font-poppins font-normal text-[3vh]">Award: </h3>
