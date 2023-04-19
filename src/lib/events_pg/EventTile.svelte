ceac76414d96ed599b66862d4efea2da1faa2609
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-04-20 01:24:24 +0530

    minor changes to eventtile styling

--- src/lib/events_pg/EventTile.svelte
+++ src/lib/events_pg/EventTile.svelte
@@ -13,8 +13,8 @@
 	
 	<div class=" overflow-hidden group-[imghov] relative m-6 pt-0 h-[650px] border-2 max-w-[400px] w-fit border-yellow-600   hover:scale-110  transition-all duration-300 delay-100 ease-in-out rounded-2xl flex justify-between flex-col items-center text-center text-white">
 		<div class=" relative flex w-full	justify-between mb-2">
-			<div class="relative pt-6 z-[1] ">
-				<img/> 
+			<div class="relative pt-6 -z-[1] ">
+				<img src={SolsticeFullLogo}> 
 			</div>
 			<div class=" relative z-[1] h-0 w-0 mr-1 capitalize flex justify-center items-end border-[30px] border-t-[50px] border-x-[32px] bg-black bg-opacity-20 border-b-transparent {data.pass === 'flagship'? "border-[#9a0b0b]": data.pass==='standard'? "border-[#0e5f0e]":"border-blue-600"}" >
 				<p class="text-[12px] -translate-y-3 font-bold">{data.pass}</p>
