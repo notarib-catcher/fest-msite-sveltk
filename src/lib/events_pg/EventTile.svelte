684c704743b12d58531ac6c2208f89d2da6356e5
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-04-20 00:55:07 +0530

    restyled event tiles

--- src/lib/events_pg/EventTile.svelte
+++ src/lib/events_pg/EventTile.svelte
@@ -11,26 +11,33 @@
 		 import{goto} from '$app/navigation';
 	</script>
 	
-	<div class="m-6 pt-0 h-[650px] p-5 border-2 max-w-[400px] w-fit border-yellow-600 hover:bg-[#303033] hover:scale-110  transition-all duration-300 delay-100 ease-in-out rounded-2xl flex justify-between flex-col items-center text-center text-white">
-		<div class="flex w-full	justify-between mb-2">
-			<div class="pt-6">
-				<img src={SolsticeFullLogo} alt="SolsticeLogoMITBLR" /> 
+	<div class=" overflow-hidden group-[imghov] relative m-6 pt-0 h-[650px] border-2 max-w-[400px] w-fit border-yellow-600   hover:scale-110  transition-all duration-300 delay-100 ease-in-out rounded-2xl flex justify-between flex-col items-center text-center text-white">
+		<div class=" relative flex w-full	justify-between mb-2">
+			<div class="relative pt-6 z-[1] ">
+				<img/> 
 			</div>
-			<div class="h-0 w-0 flex justify-center items-end border-[17px] border-t-[22px] border-x-[30px] border-b-transparent {data.pass === 'flagship'? "border-red-600": data.pass==='standard'? "border-green-600":"border-blue-600"}" >
-				<p class="text-[12px]">{data.pass}</p>
+			<div class=" relative z-[1] h-0 w-0 mr-1 capitalize flex justify-center items-end border-[30px] border-t-[50px] border-x-[32px] border-b-transparent {data.pass === 'flagship'? "border-red-600": data.pass==='standard'? "border-green-600":"border-blue-600"}" >
+				<p class="text-[12px] -translate-y-3">{data.pass}</p>
 			</div>
 		</div>
-		<img src={data.image} alt="eventImage" class="rounded-3xl m-2 h-[165px]" width={220}/>
+		<img src={data.image} alt="eventImage" class=" w-full h-[230px] group-[imghov] absolute -z-1" width={220}/>
 		<p class="text-2xl font-bold">{data.title}</p>
-		<div class="w-[220px] m-2 text-xl font-light">
-			<p>{data.shortDescription}</p>
+		<div class="relative w-full h-full bgshadow p-5  transition-all duration-300 peer">
+			<div class="w-[220px] m-2 text-xl translate-y-24 font-light relative z-10 ">
+				<p>{data.shortDescription}</p>
+			</div>
 		</div>
+		
 		<div class="w-full flex justify-end mt-2">
-			<button class=" bg-gradient-to-tr from-[#D2691E] to-[#FFD700] hover:bg-blue-700 text-black font-sans py-2 px-4 rounded-xl" on:click={()=>{
+			<button class=" mx-auto mb-4 bg-gradient-to-tr from-[#4c2509] to-[#75660f] hover:bg-blue-700 transition-all duration-200 text-white font-light py-2 px-4 rounded-lg" on:click={()=>{
 				goto('/events/'+data._id)
-			}}>Register</button>
+			}}>Learn More</button>
 		</div>
 	</div>
 	
-	
+	<style>
+		.bgshadow{
+			background-image: linear-gradient(to top, rgba(24,24,27,1),rgba(24,24,27,1),rgba(24,24,27,1),rgba(24,24,27,1),rgba(24,24,27,1),rgba(24,24,27,0));
+		}
+	</style>
 	
