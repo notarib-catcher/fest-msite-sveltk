5b0e87488c9c290c6b564fce5865c07c2cc4d388
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-04-21 00:42:14 +0530

    Removed gradient BG from bganim

--- src/lib/common/BGAnim.svelte
+++ src/lib/common/BGAnim.svelte
@@ -1,5 +1,9 @@
-<div class="area bg-anim min-h-screen w-screen fixed left-0 top-0 -z-[2]">
+<script>
+	import bgimgstars from "../../assets/starsbg.svg"
+</script>
 
+<div class="area min-h-screen w-screen fixed left-0 top-0 -z-[2]">
+	<img src = {bgimgstars} class="w-screen min-h-screen blur-[1px] absolute left-0 top-0">
 </div>
 <style>
  
