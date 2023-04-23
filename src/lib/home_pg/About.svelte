b314a5f3172caeb0424ee5676323da3e1729904b
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-04-23 18:54:23 +0530

    Reformatted edgy intro text for edgy designer

--- src/lib/home_pg/About.svelte
+++ src/lib/home_pg/About.svelte
@@ -5,10 +5,10 @@
       </div>
     <p class=" mt-10 text-lg w-[70vw] max-lg:w-[80vw]  from-[#f1a9d9] text-center to-[#e67ec3] via-[#9879d1] bg-clip-text text-transparent bg-gradient-to-tr">
 
-This is no ordinary fest. This is a fest curated by a team that hasn't slept for days, a team that's surviving on coffee just to make sure that the Bangalore's Tech Circuit gets the finest of the events possible. 
+<b>This is no ordinary fest.</b><br><br>This is a fest curated by a team that hasn't slept for days, a team that's surviving on coffee just to make sure that the Bangalore's Tech Circuit gets the finest of the events possible. 
 Tech Solstice brings to you a platform to compete with the lot that follows Linus as enthusiastically as you do. Both Sebastian and Torvalds.
 From E-sports to Hackathons, everyone can find an event right for their interests.<br><br> 
 Join us for mind bending events that make you wanna rip your head off.<br><br class=" lg:hidden">Join us for the amazing concert and DJ nights.<br><br class=" sm:hidden">
-Join us to code, quiz or debate.<br class=" sm:hidden"><br><b>Join us to not be ordinary.</b>
+Join us to code, quiz or debate.<br><br><b>Join us to not be ordinary.</b>
     </p>
 </div>
