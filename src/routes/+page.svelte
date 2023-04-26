caf643f980f370646a30ac74b95221df72157d11
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-04-26 21:06:01 +0530

    Remove unused import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import BgAnim from "$lib/common/BGAnim.svelte";
     import Map from "$lib/home_pg/Map.svelte";
     import About from "$lib/home_pg/About.svelte";
-	import Team from "$lib/home_pg/Team.svelte";
     import Sponsors from "$lib/home_pg/Sponsors.svelte";
 </script>
 
