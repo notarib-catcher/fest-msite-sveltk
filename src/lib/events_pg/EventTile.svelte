253a2f0bea8a467e403c532779f75c11aa486155
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-09-25 05:31:46 +0530

    Restyled event main page, integrated with strapi

--- src/lib/events_pg/EventTile.svelte
+++ src/lib/events_pg/EventTile.svelte
@@ -1,117 +1,130 @@
 <script>
-	// @ts-nocheck
+
 
 	/**
-	 * @type {{ image: any; }}
+	 * @type {string}
 	 */
-	export let data;
-	import SolsticeFullLogo from '../../../src/assets/SolsticeFullLogo.svg';
-	import { goto } from '$app/navigation';
+	export let eventID
+	/**
+	 * @type {string}
+	 */
+	export let shortDesc
+
+	/**
+	 * @type {string} 
+	 * Requires one of these values:
+	 * 	dark: Black text & Heading, 
+	 * 	light: White text & Heading,
+	 * 	alt-dark: Black heading & White text  
+	 * 	alt-light: White heading & Black text
+	 */
+	export let cscheme
+
+	/**
+	 * @type {string}
+	 */
+
+	export let heading
+
+	/**
+	 * @type {string}
+	 */
+	export let imageURL
+
 </script>
+{#if cscheme == "dark"}
 
-<div
-	class=" border-[2px] {
-			data.forceTop?"border-[#26e2faaf]"
-			: data.pass ==='Flagship'
-			? 'border-[#f0efed]'
-			: data.pass === 'Standard'
-			? 'border-[#2564eb9c] '
-			: data.pass == 'Proshow'
-			? 'border-[#21adc5cb]'
-			:  data.pass == 'Esports'
-			? 'border-[#e63c3c]'
-			: data.pass == 'Free'
-			? 'border-[#18ff189b]'
-			: 'border-[#0e5f0e9b]'} borderbox   overflow-hidden group-[imghov] relative md:m-6  my-6 pt-0 w-[291px] h-[547px]  hover:scale-105 transition-all duration-300  ease-in-out rounded-2xl flex justify-between flex-col items-center text-center text-white"
->
-	<div class=" relative flex w-full justify-between mb-2">
-		<div class="relative pt-6 pl-4 -z-[1]">
-			<img src={SolsticeFullLogo} alt="logo" />
-		</div>
-
-		
-			<p class="text-[18px] mt-6 mr-4 font-bold {
-				data.pass ==='Flagship'
-				? 'from-[#125ddf]  to-[#F365E5] bg-clip-text text-transparent bg-gradient-to-r'
-				: data.pass === 'Standard'
-				? 'text-[#2564eb9c] '
-				: data.pass == 'Proshow'
-				? 'text-[#e5d016]'
-				:  data.pass == 'Esports'
-				? 'text-[#e63c3c]'
-				: data.pass == 'Free'
-				? 'text-[#18ff189b]'
-				: 'text-[#0e5f0e9b]'}">{data.pass}</p>
-		
+<div class="text-black font-yatra w-[225px] h-[400px] relative rounded-lg overflow-clip z-0 hover:z-[1] hover:scale-105 focus:z-[1] focus:scale-105 transition-all duration-200 bg-[#aaaaaa]">
+	<!-- svelte-ignore a11y-missing-attribute -->
+	<img class=" w-full h-full left-0 top-0 absolute z-[-1]" src={imageURL}>
+	<div class="z-1 w-full h-[50px] top-0 absolute text-center p-2 text-xl font-semibold">
+		{heading}
 	</div>
-	<img
-		src={data.image}
-		alt="eventImage"
-		class="  h-[165px] w-[220px] rounded-xl top-16 group-[imghov] absolute -z-1"
-		width={220}
-	/>
-	<div class=" relative flex justify-center w-full h-full bg-transparent p-5 transition-all duration-300 peer">
-		<div class="w-[200px] pointer-events-none max-w-full m-2 text-[17.5px]  translate-y-40 font-light relative z-[2]">
-			<p>{data.shortDescription}</p>
+	<div class="absolute w-full h-[50%] bg-black bottom-0 bg-opacity-30">
+		<div class="absolute top-5 p-5 text-center">
+			<span class=" w-full ">
+				{shortDesc || "Click the button for more details!"}
+			</span>
+		</div>
+		<div class=" absolute bottom-5 w-full flex items-center justify-center">
+			<button class=" p-2 border-black rounded-md border-2 hover:scale-105 active:scale-95 focus:scale-105 transition-all duration-200" on:click={() => {window.location = "/events/" + eventID}}>
+				More info
+			</button>
 		</div>
 	</div>
-
-	<div class="flex justify-end z-[1] bottom-0 flex-col text-center ">
-		{#if data.prizepool != undefined}
-			<div class="mb-1 w-full">
-				<p class="text-white font-normal">Prize pool: {data.prizepool}</p>
-			</div>
-		{/if}
-		{#if data.team != undefined}
-			<div class="w-full flex justify-center mb-2">
-				<p class="text-white font-normal">Team: {data.team}</p>
-			</div>
-		{/if}
-		<a
-			href={'/events/' + data._id}
-			class=" border-[2px] {
-				data.forceTop?"border-[#26e2faaf] hover:bg-[#26e2faaf]"
-				: data.pass ==='Flagship'
-				? 'border-[#f0efed] hover:bg-[#f0efed]'
-				: data.pass === 'Standard'
-				? 'border-[#2564eb9c] hover:bg-[#2564eb9c]'
-				: data.pass == 'Proshow'
-				? 'border-[#21adc5cb] hover:bg-[#21adc5cb]'
-				:  data.pass == 'Esports'
-				? 'border-[#e63c3c] hover:bg-[#e63c3c]'
-				: data.pass == 'Free'
-				? 'border-[#19a3199b] hover:bg-[#19a3199b]'
-				: 'border-[#0e5f0e9b] hover:bg-[#0e5f0e9b]'} mx-auto mb-8  to-[#ffd900c7]  bg-opacity-10 transition-all duration-200 text-white font-normal py-2 px-4 rounded-lg"
-			>Learn More</a
-		>
-	</div>
+	
 </div>
 
+{:else if cscheme == "light"}
+
+<div class="text-white font-yatra w-[225px] h-[400px] relative rounded-lg overflow-clip z-0 hover:z-[1] hover:scale-105 focus:z-[1] focus:scale-105 transition-all duration-200 bg-[#aaaaaa]">
+	<!-- svelte-ignore a11y-missing-attribute -->
+	<img class=" w-full h-full left-0 top-0 absolute z-[-1]" src={imageURL}>
+	<div class="z-1 w-full h-[50px] top-0 absolute text-center p-2 text-xl font-semibold">
+		{heading}
+	</div>
+	<div class="absolute w-full h-[50%] bg-black bottom-0 bg-opacity-30">
+		<div class="absolute top-5 p-5 text-center">
+			<span class=" w-full ">
+				{shortDesc || "Click the button for more details!"}
+			</span>
+		</div>
+		<div class=" absolute bottom-5 w-full flex items-center justify-center">
+			<button class=" p-2 border-white rounded-md border-2 hover:scale-105 active:scale-95 focus:scale-105 transition-all duration-200" on:click={() => {window.location = "/events/" + eventID}}>
+				More info
+			</button>
+		</div>
+	</div>
+	
+</div>
+
+{:else if cscheme == "alt-dark"}
+
+<div class="font-yatra w-[225px] h-[400px] relative rounded-lg overflow-clip z-0 hover:z-[1] hover:scale-105 focus:z-[1] focus:scale-105 transition-all duration-200 bg-[#aaaaaa]">
+	<!-- svelte-ignore a11y-missing-attribute -->
+	<img class=" w-full h-full left-0 top-0 absolute z-[-1]" src={imageURL}>
+	<div class="text-black z-1 w-full h-[50px] top-0 absolute text-center p-2 text-xl font-semibold">
+		{heading}
+	</div>
+	<div class="text-white absolute w-full h-[50%] bg-black bottom-0 bg-opacity-30">
+		<div class="absolute top-5 p-5 text-center">
+			<span class=" w-full ">
+				{shortDesc || "Click the button for more details!"}
+			</span>
+		</div>
+		<div class=" absolute bottom-5 w-full flex items-center justify-center">
+			<button class=" p-2 border-white rounded-md border-2 hover:scale-105 active:scale-95 focus:scale-105 transition-all duration-200" on:click={() => {window.location = "/events/" + eventID}}>
+				More info
+			</button>
+		</div>
+	</div>
+	
+</div>
+
+{:else if cscheme == "alt-light"}
+
+<div class=" font-yatraw-[225px] h-[400px] relative rounded-lg overflow-clip z-0 hover:z-[1] hover:scale-105 focus:z-[1] focus:scale-105 transition-all duration-200 bg-[#aaaaaa]">
+	<!-- svelte-ignore a11y-missing-attribute -->
+	<img class=" w-full h-full left-0 top-0 absolute z-[-1]" src={imageURL}>
+	<div class="text-white z-1 w-full h-[50px] top-0 absolute text-center p-2 text-xl font-semibold">
+		{heading}
+	</div>
+	<div class="absolute w-full h-[50%] bg-black bottom-0 bg-opacity-30">
+		<div class="absolute top-5 p-5 text-center">
+			<span class=" w-full ">
+				{shortDesc || "Click the button for more details!"}
+			</span>
+		</div>
+		<div class=" absolute bottom-5 w-full flex items-center justify-center">
+			<button class=" p-2 border-black rounded-md border-2 hover:scale-105 active:scale-95 focus:scale-105 transition-all duration-200" on:click={() => {window.location = "/events/" + eventID}}>
+				More info
+			</button>
+		</div>
+	</div>
+	
+</div>
+
+{/if}
 <style>
-	.bgshadow {
-		background-image: linear-gradient(
-			to top,
-			rgba(24, 24, 27, 1),
-			rgba(24, 24, 27, 1),
-			rgba(24, 24, 27, 1),
-			rgba(24, 24, 27, 6),
-			rgba(24, 24, 27, 0.7),
-			rgba(24, 24, 27, 0)
-		);
-	}
 
-	
-
-	.borderbox:hover{
-		box-shadow: 0px 0px 30px rgba(255, 255, 255, 0.685);
-	}
-
-	.borderbox:focus{
-		box-shadow: 0px 0px 30px rgba(255, 255, 255, 0.685);
-	}
-
-	.borderbox:focus-within{
-		box-shadow: 0px 0px 30px rgba(255, 255, 255, 0.685);
-	}
-	
 </style>
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,160 +1,98 @@
 <script>
-	// @ts-nocheck
+	import { browser } from '$app/environment';
 
+	// @ts-nocheck
+	const txtcdnURL = "https://txtcdn.mitblrfest.in/api/events"
 	import BgAnim from '$lib/common/BGAnim.svelte';
 	import EventTile from '$lib/events_pg/EventTile.svelte';
-	// import { Container } from 'postcss';
-	// import { each, onMount } from 'svelte/internal';
+	import About from '$lib/home_pg/About.svelte';
+	import { onMount } from 'svelte';
+	import { each } from 'svelte/internal';
 
-	// let particles = 300
-	// let m = {x: 0,y:0}
-	// let multiplier = -10
+	let FeaturedEvents = []
+	let SportsEvents = []
+	let CulturalEvents = []
+	let EsportsEvents = []
 
-	// function newanimate(){
+	onMount(async () => {
+		if(browser){
+			let eventData = (await (await fetch(txtcdnURL)).json()).data
+			
+			if(!eventData){
+				console.warn("[FETCH] No event data found")
+			}
 
-	//   var root = document.querySelector(':root');
-	//   root.style.setProperty('--x', `${Math.random()*window.innerWidth}%`);
-	//   root.style.setProperty('--y', `${Math.random()*window.innerHeight}%`);
-	// }
+			let FeaturedEventsTemp = []
+			let SportsEventsTemp = []
+			let CulturalEventsTemp = []
+			let EsportsEventsTemp = []
+			
+			for(let festEvent of eventData){
+				console.log(festEvent.attributes.Priority)
+				if(festEvent.attributes.Priority <= 1000){
+					FeaturedEventsTemp.push(festEvent)
+				}
+				else if(festEvent.attributes.Priority <= 2000){
+					SportsEventsTemp.push(festEvent)
+				}
+				else if(festEvent.attributes.Priority <=3000){
+					CulturalEventsTemp.push(festEvent)
+				}
+				else{
+					EsportsEventsTemp.push(festEvent)
+				}
+			}
 
-	//   function handleMouse(event){
-	//     m.x = multiplier*event.clientX;
-	//     m.y = multiplier*event.clientY;
-	//     var root = document.querySelector(':root');
-	//     root.style.setProperty('--x', `${m.x}%`);
-	//     root.style.setProperty('--y', `${m.y}%`);
-	//   }
-	// onMount(()=>{
-	//   var countainer = document.getElementById("stars")
-	//   for(let i =0;i<particles;i++)
-	//   countainer.innerHTML += `<div class=" bg-white h-[3px] w-[3px] rounded-full blur-[1px] absolute ease-in-out s-N-9Ekhs3dG6k" style="top: ${Math.random()*window.innerHeight*1.5}px;left: ${Math.random()*innerWidth*1.5}px;opacity:${Math.random()}" id="star"></div>`
-	// })
+			FeaturedEvents = FeaturedEventsTemp
+			SportsEvents = SportsEventsTemp
+			CulturalEvents = CulturalEventsTemp
+			EsportsEvents = EsportsEventsTemp
 
-	export let data;
+		}
+	})
 
-	// let topevents = data.events.filter((event) => {
-	// 	return event.pass == 'Flagship' || event.pass == 'Proshow' || event._id == 'hacksolstice';
-	// });
-
-	// let band = topevents.pop();
-	// let hackathon = topevents.pop();
-	// let dj = topevents.pop();
-	// // adds element to the begining of the array
-	// topevents.unshift(dj);
-	// topevents.unshift(band);
-	// topevents.unshift(hackathon);
-	// //  let hackathon = topevents.pop()
-	// //  let dj = topevents.pop()
-	// //  let thunt = topevents.pop()
-
-	// //  topevents.unshift(dj)
-	// //  topevents.unshift(thunt)
-	// //  topevents.unshift(hackathon)
-
-	// for (let i in topevents) {
-	// 	topevents[i].forceTop = true;
-	// }
-
-	// // all the events except the ones included in topevent
-	// let remainingevents1 = data.events.filter((event) => {
-	// 	return !topevents.includes(event);
-	// });
-
-	// let freeevents = remainingevents1.filter((event) => {
-	// 	return event.pass.toLowerCase() == 'free';
-	// });
-
-	// // all the events except the ones included in topevents and freeevents
-	// let remainingevents2 = remainingevents1.filter((event) => {
-	// 	return !freeevents.includes(event);
-	// });
-
-	// let standard = remainingevents2.filter((event) => {
-	// 	return event.pass.toLowerCase() == 'standard';
-	// });
-
-	// let esports = remainingevents2.filter((event) => {
-	// 	return !standard.includes(event);
-	// });
 </script>
 
 <BgAnim />
-<div>
-	<!-- <div class="stars  h-full fixed -z-10 w-full stars" id="stars">
-      <div class="bg-white h-1 w-1 rounded-full blur-[2px] absolute" style="top: 90px;left:90px" id="star"></div>
-    </div> -->
-	<div class="pt-16 max-md:pt-5 text-white">
-		{#each Object.entries(data.priorityCategories) as [name, info]}
-			<h1>{info.displayName}</h1>
-			{#each data.events as event}
-				{#if event.priority >= info.from && event.priority <= info.to}
-					<h2>&emsp;&nbsp;Event Name -> {event.eventName}</h2>
-					<h2>&emsp;&emsp;&nbsp;Event Description -> {event.eventDescription}</h2>
-				{/if}
+<div class="h-fit w-screen flex mt-10 items-center justify-center flex-col p-10">
+	
+	<div class=" w-fit h-fit">
+		<div class=" text-6xl w-full text-center md:text-left mb-10 text-[#FE786F] font-yatra">
+			Featured Events
+		</div>
+		<div class=" grid grid-flow-row gap-16  lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xl:grid-cols-5 xl: ">
+			{#each FeaturedEvents as festEvent}
+				<EventTile eventID={festEvent.attributes.EventID} cscheme={festEvent.attributes.Cscheme} imageURL={festEvent.attributes.PImage} heading={festEvent.attributes.Heading} shortDesc={festEvent.attributes.ShortDescription}></EventTile>
 			{/each}
-		{/each}
+		</div>
+		<div class="mt-10 text-6xl w-full text-center md:text-left mb-10 text-[#0ebaaa] font-yatra">
+			Sports Events
+		</div>
+		<div class=" grid grid-flow-row gap-16  lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xl:grid-cols-5 xl: ">
+			{#each SportsEvents as festEvent}
+				<EventTile eventID={festEvent.attributes.EventID} cscheme={festEvent.attributes.Cscheme} imageURL={festEvent.attributes.PImage} heading={festEvent.attributes.Heading} shortDesc={festEvent.attributes.ShortDescription}></EventTile>
+			{/each}
+		</div>
+		<div class="mt-10 text-6xl w-full text-center md:text-left mb-10 text-[#1095ff] font-yatra">
+			Cultural Events
+		</div>
+		<div class=" grid grid-flow-row gap-16  lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xl:grid-cols-5 xl: ">
+			{#each CulturalEvents as festEvent}
+				<EventTile eventID={festEvent.attributes.EventID} cscheme={festEvent.attributes.Cscheme} imageURL={festEvent.attributes.PImage} heading={festEvent.attributes.Heading} shortDesc={festEvent.attributes.ShortDescription}></EventTile>
+			{/each}
+		</div>
+		<div class="mt-10 text-6xl w-full text-center md:text-left mb-10 text-[#ed008c] font-yatra">
+			E-Sports Events
+		</div>
+		<div class=" grid grid-flow-row gap-16  lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xl:grid-cols-5 xl: ">
+			{#each EsportsEvents as festEvent}
+				<EventTile eventID={festEvent.attributes.EventID} cscheme={festEvent.attributes.Cscheme} imageURL={festEvent.attributes.PImage} heading={festEvent.attributes.Heading} shortDesc={festEvent.attributes.ShortDescription}></EventTile>
+			{/each}
+		</div>
 	</div>
-	<!-- <div class=" pt-16 max-md:pt-5 md:px-20 max-w-full flex flex-col justify-center left-0 min-w-min">
-		<div class="w-full flex flex-col lg:flex-row">
-			<h1
-				class="text-6xl max-md:text-3xl mb-4 font-bold bg-gradient-to-tr from-[#3BACC1] via-[#2D6DB1] to-[#3BACC1] text-transparent w-full max-lg:text-center bg-clip-text"
-			>
-				Top Events
-			</h1>
-			<div class=" font-semibold w-full lg:text-right max-md:text-lg text-center text-xl">
-				<a
-					class=" border-b-2 hover:border-[#2e75b3] border-[#7945bd] hover:from-[#3BACC1] hover:via-[#2D6DB1] hover:to-[#3BACC1] hover:bg-gradient-to-r hover:bg-clip-text from-[#D283B8] to-[#D283B8] via-[#6738BE] bg-gradient-to-r bg-clip-text focus:from-[#3BACC1] focus:via-[#2D6DB1] focus:to-[#3BACC1] focus:bg-gradient-to-r focus:bg-clip-text duration-100 font-semibold hover:font-bold underline text-transparent pt-3"
-					href="https://cdn.mitblrfest.in/final.pdf">You can find the final schedule here</a
-				>.
-			</div>
-		</div>
-
-		<div class="flex flex-row flex-wrap justify-center items-center">
-			{#each topevents as event}
-				{#if !event.hide}
-					<EventTile data={event} />
-				{/if}
-			{/each}
-		</div>
-
-		<h1
-			class="text-6xl max-md:text-3xl mt-10 mb-4 font-bold bg-gradient-to-tr from-[#26bf87] via-[#17d337] to-[#26bf87] text-transparent w-full max-lg:text-center bg-clip-text"
-		>
-			Open to all
-		</h1>
-		<div class="flex flex-row flex-wrap justify-center items-center">
-			{#each freeevents as event}
-				{#if !event.hide}
-					<EventTile data={event} />
-				{/if}
-			{/each}
-		</div>
-		<h1
-			class="text-6xl max-md:text-3xl mt-10 mb-4 font-bold bg-gradient-to-tr from-[#6212a4] via-[#2D6DB1] to-[#6212a4] text-transparent w-full max-lg:text-center bg-clip-text"
-		>
-			Fun Mini events
-		</h1>
-		<div class="flex flex-row flex-wrap justify-center items-center">
-			{#each standard as event}
-				{#if !event.hide}
-					<EventTile data={event} />
-				{/if}
-			{/each}
-		</div>
-		<h1
-			class="text-6xl max-md:text-3xl mt-10 mb-4 font-bold bg-gradient-to-tr from-[#880cae] via-[#9c434b] to-[#880cae] text-transparent w-full max-lg:text-center bg-clip-text"
-		>
-			Esports Competitions
-		</h1>
-		<div class="flex flex-row flex-wrap justify-center items-center">
-			{#each esports as event}
-				{#if !event.hide}
-					<EventTile data={event} />
-				{/if}
-			{/each}
-		</div> -->
-	<!-- </div> -->
+	
 </div>
+
 
 <style>
 	/* :root{
