c4a3a530d5f7f238c862ad7a9c31cc4ff2ffa077
Kshitij Srivastava <87322594+k-srivastava@users.noreply.github.com>
2023-09-25 17:46:46 +0530

    Created a component to parse markdown strings into raw HTML.

---
+++ src/lib/events_pg/MDConverter.svelte
@@ -0,0 +1,24 @@
+<script>
+    import marked from "marked";
+
+    /**
+     * Markdown source to be parsed into raw HTML.
+     * @type string
+     */
+    export let markdown;
+
+    /**
+     * Convert a markdown source into a raw HTML string.
+     * @example ```{@html toHTML(markdown)}```
+     *
+     * @param {string} markdown
+     * @return string
+     */
+    function toHTML(markdown) {
+        return marked.marked(markdown);
+    }
+</script>
+
+<div class="md-converter">
+    {@html toHTML(markdown)}
+</div>
