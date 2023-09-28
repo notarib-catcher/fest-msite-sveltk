920cd555df15bc24ee27317bfcd3f4c7cac9bfd0
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-09-28 20:54:39 +0530

    Reduced opacity of lightning

--- src/lib/common/BGAnim.svelte
+++ src/lib/common/BGAnim.svelte
@@ -3,7 +3,7 @@
 	import bgbkdlightning from "../../assets/Group 28lightning.png"
 </script>
 <img src={bgimgtrees} class=" -z-[199] w-screen fixed bottom-0 left-0 imfilter">
-<img src={bgbkdlightning} class=" -z-[200] w-screen fixed top-0 left-0 -translate-y-[10%]">
+<img src={bgbkdlightning} class=" -z-[200] w-screen fixed top-0 left-0 -translate-y-[10%] opacity-60">
 
 <div class="fixed top-0 left-0 h-[100dvh] w-screen bg-[#3900bd] -z-[201] brightness-50">
 	
