914d2621388c56c9cd0c52cbf1990ea72228edf4
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-10-01 01:40:39 +0530

    Updates to event structure

--- src/routes/events/[id]/+page.server.js
+++ src/routes/events/[id]/+page.server.js
@@ -26,31 +26,37 @@ const allowedPasses = {
     "S_TT_F" : ["SPORT_TT_F"],
     "S_ATH" : ["SPORT_ATH"],
     "S_CHS" : ["SPORT_CHS"],
-    "C_PRO" : ["CLTR_PRO"], //No need to register
-    "C_BOB" : ["CLTR_BOB"], //Team event
-    "C_GRD" : ["CLTR_GRD"], //Team event
-    "C_FAS" : ["CLTR_FAS"],
-    "C_STRSPL" : ["CLTR_PRO"],
-    "C_THESHD" : ["CLTR_PRO"],
-    "C_MNDSCR" : ["CLTR_PRO"],
-    "C_MKTMHM" : ["CLTR_PRO"],
-    "C_RANCOM" : ["CLTR_PRO"],
-    "C_BTBLBD" : ["CLTR_PRO"],
-    "C_NATNIR" : ["CLTR_PRO"],
-    "C_FCSFRM" : ["CLTR_PRO"],
-    "C_RMXRYL" : ["CLTR_PRO"],
-    "C_MNGODY" : ["CLTR_PRO"],
-    "C_BSHBTL" : ["CLTR_PRO"],
-    "C_QZAPLZ" : ["CLTR_PRO"],
-    "C_ZENITH" : ["CLTR_PRO"],
-    "C_STRBET" : ["CLTR_PRO"],
-    "C_BRNWAV" : ["CLTR_PRO"],
-    "C_SAARANG" : ["CLTR_PRO"],
-    "ES_BGDMHM" : ["ESPORTS"],
-    "ES_VALRIS" : ["ESPORTS"],
-    "ES_CODEXE" : ["ESPORTS"],
-    "ES_CRWQST" : ["ESPORTS"],
-    "ES_GOLQST" : ["ESPORTS"]
+    
+    //Cultural Featured
+    "C_PRO" : ["CLTR_PRO", "SUP_PRO"], //no regs
+    "C_BOB" : ["CLTR_BOB"], //team, 8
+    "C_GRC" : ["CLTR_GRD"], //team, 15
+    "C_GRW" : ["CLTR_GRD"], //team, 20
+    "C_FAS" : ["CLTR_FAS"], //team, 10
+
+    //Cultural Normie
+    "C_MNDSCR": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_BTSCAR": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_STRBET": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_BTBLBD": ["CLTR_PRO", "SUP_PRO"], //team, 4
+    "C_FCSFRM": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_ZENITH": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_SHKTNK": ["CLTR_PRO", "SUP_PRO"], //team, 7
+    "C_BSHBTL": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_MNGODY": ["CLTR_PRO", "SUP_PRO"], //solo
+    "C_RANCOM": ["CLTR_PRO", "SUP_PRO"], //team, 3
+    "C_THESHD": ["CLTR_PRO", "SUP_PRO"], //team, 20
+    "C_STRSPL": ["CLTR_PRO", "SUP_PRO"], //team, 20
+    "C_MKTMHM": ["CLTR_PRO", "SUP_PRO"], //team, 3
+    "C_CINCRF": ["CLTR_PRO", "SUP_PRO"], //team, 4
+    "C_BTLBAB": ["CLTR_PRO", "SUP_PRO"], //team, 2
+
+    //Esports
+    "ES_BGDMHM" : ["ESPORTS"], //team, 4
+    "ES_VALRIS" : ["ESPORTS"], //team, 7
+    "ES_CODEXE" : ["ESPORTS"], //team, 5
+    "ES_CRWQST" : ["ESPORTS"], //team, 2
+    "ES_GOLQST" : ["ESPORTS"]  //solo
 }
 
 
