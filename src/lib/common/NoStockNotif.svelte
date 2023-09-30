aaa7e8189e98efa3991316645cc7c4f08964f91c
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-09-30 15:28:00 +0530

    Added a notification for when tickets are sold out (for early bird passes)

---
+++ src/lib/common/NoStockNotif.svelte
@@ -0,0 +1,45 @@
+<script>
+import { browser } from '$app/environment';
+    import {page} from '$app/stores'
+
+    const isOutOfStock = $page.url.searchParams.has('nostock')
+    if(browser){
+        setTimeout(() => {
+            window.history.pushState({}, document.title, window.location.pathname);
+        }, 1000)
+    }
+</script>
+
+<div class = "absolute top-0 w-screen h-fit z-[2000] pointer-events-none">
+    {#if isOutOfStock}
+    <div class="font-uni text-2xl font-semibold h-[50px] w-fit p-2 bg-[#FE786F] text-white border-[1px] border-white mx-auto ModalPopIn">
+        Sold out! Come back tomorrow
+    </div>
+    {/if}
+</div>
+
+<style>
+
+.ModalPopIn{
+    transform: translateY(-200px);
+    animation-name: login;
+    animation-duration: 3000ms;
+    animation-iteration-count: 1;
+    animation-fill-mode: forward;
+    animation-timing-function: ease-in-out;
+    }
+    @keyframes login{
+        0%{
+            transform: translateY(-55px);
+        }
+        25%{
+            transform: translateY(20px);
+        }
+        50%{
+            transform: translateY(20px);
+        }
+        100%{
+            transform: translateY(-200px);
+        }
+    }
+</style>
