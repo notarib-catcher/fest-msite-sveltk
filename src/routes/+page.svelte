75f28e8e51b943c5cf27b3b950898cc4134a62f8
ribcatcher <15999680+notarib-catcher@users.noreply.github.com>
2023-10-03 22:24:22 +0530

    New homepage banner done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,32 @@
 <script>
-    import LandingPage from "$lib/home_pg/LandingPage.svelte";
-    
+     
 </script>
+<div class=" h-fit bg-[#3300bd] -z-[2] relative">
+    <div class="h-[100lvh] w-screen bg-opacity-100 ">
+        <div class=" bannertop absolute top-0 left-0 w-screen h-[70vh] -z-[1]">  
+        </div>
+        <div class=" absolute w-full h-full top-0 left-0 flex items-center justify-center">
+            <div class=" text-[14vw] font-monster bg-clip-text text-transparent bg-gradient-to-b from-[#FFE500] to-[#F9BAA8]">
+                FALAK SURGE
+            </div>
+        </div>
+        <div class=" bannerbtm absolute bottom-0 left-0 w-screen h-[60vh] z-[1]">
+    
+        </div>
+    </div>
+</div>
 
-<LandingPage />
+
+<style>
+    .bannertop{
+        background-image: url("/imgs/lghtnn.png");
+        background-position: 0px -50px;
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+    .bannerbtm{
+        background-image: url("/imgs/trees.png");
+        background-size: cover;
+        background-repeat: repeat-x no-repeat;
+    }
+</style>
